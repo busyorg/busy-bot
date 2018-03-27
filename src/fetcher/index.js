856181b974f5c6e84ef63c896d37f70b579a7132
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2018-03-27 22:19:48 +0200

    refactor: extract initializer

--- src/fetcher/index.js
+++ src/fetcher/index.js
@@ -1,15 +1,15 @@
-const debug = require('debug')('busy-bot:worker');
+const debug = require('debug')('busy-bot:fetcher');
 const RSMQWorker = require('rsmq-worker');
-
 const { FETCHERS_QUEUE } = require('../constants');
 
 function start() {
+  debug('fetcher started');
   const worker = new RSMQWorker(FETCHERS_QUEUE);
-  worker.start();
   worker.on('message', function(msg, next, id) {
     debug('Received message: ' + id);
     next();
   });
+  worker.start();
 }
 
 module.exports = {
